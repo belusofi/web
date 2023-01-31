201b3ac9e0e05595fee92735eb942c194e7ee048
Sofi <belusofi17@gmail.com>
2023-01-31 02:22:18 -0500

    proyecto web

---
+++ src/main/java/com/distribuida/App.java
@@ -0,0 +1,52 @@
+package com.distribuida;
+
+import com.distribuida.Thymeleaf.ThymeleafUtil;
+import com.distribuida.models.Author;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+
+import spark.ModelAndView;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+public class App {
+    public static void main(String[] args) {
+        //CloseableHttpClient client = HttpClients.createDefault();
+        //HttpGet req = new HttpGet("http://localhost:8081/authors");
+        port(8900);
+            get("/", (request, response) -> "Pagina principal");
+
+        get("/author/listar", (req, res) -> {
+
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("today", new Date());
+            System.out.println("");
+            IContext context = new Context(req.raw().getLocale(), variables);
+            String out = ThymeleafUtil.getTemplateEngine().process("author_listar", context);
+
+            return out;
+        });
+//agregar
+        get("/author/add", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            Author author = new Author();
+            model.put("author", author);
+
+            return new ThymeleafTemplateEngine().render(
+                    new ModelAndView(model, "author_new")
+            );
+        });
+
+
+    }
+}
---
+++ src/main/java/com/distribuida/ClienteMp01.java
@@ -0,0 +1,24 @@
+package com.distribuida;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class ClienteMp01 {
+
+    /*public static void main(String[] args) throws Exception {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet req = new HttpGet("http://localhost:8081/authors");
+
+        var response = client.execute(req);
+
+        HttpEntity entity = response.getEntity();
+        
+        String result = EntityUtils.toString(entity);
+            System.out.println(result);
+        Gson respuesta = new Gson();
+        Type tipoListaAuthors = new TypeToken<List<Author>>(){}.getType();
+        List<Author> author = respuesta.fromJson(result, tipoListaAuthors);
+        System.out.println(author.toString());
+
+    }*/
+}
